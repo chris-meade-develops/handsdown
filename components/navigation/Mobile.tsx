011dc41fc49d2f17ef21a4705089dd60e472362f
Chris Meade <chrism@target-technology.co.uk>
2023-05-21 17:34:41 +0100

    mobile nav

---
+++ components/navigation/Mobile.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Phone } from "@/icons";
+import BurgerMenu from "./BurgerMenu";
+
+export default function Mobile({ open, onClick }: MobileNav) {
+  return (
+    <nav className="absolute top-0 left-0 w-full py-20 xl:hidden px-14 z-2">
+      <ul className="flex items-center justify-between list-none">
+        <li>
+          <BurgerMenu open={open} onClick={onClick} />
+        </li>
+        <li>
+          <a href="tel:0123456789" className="block w-13 h-13">
+            <Phone className={`w-full h-full -rotate-12 transition-colors duration-300 ${open ? "fill-offBlack" : "fill-secondary-text"}`} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
