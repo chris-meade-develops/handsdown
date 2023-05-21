46a5da0636e4a3571b4c71def26f5018f2f5e70b
Chris Meade <chrism@target-technology.co.uk>
2023-05-21 17:35:00 +0100

    navigation interfaces

---
+++ components/navigation/navigation.d.ts
@@ -0,0 +1,26 @@
+interface Clickable {
+  onClick: () => void;
+}
+
+interface Toggleable {
+  open: boolean;
+}
+
+interface NavigationItem {
+  href: string;
+  label: string;
+}
+
+interface NavigationList {
+  items?: NavigationItem[];
+}
+
+interface MobileNav extends Clickable, Toggleable {}
+
+interface BurgerMenu extends Clickable, Toggleable {}
+
+interface MobileDropDown extends Toggleable, NavigationList {}
+
+interface SideDrawer extends Toggleable {}
+
+interface MobileNavItem extends NavigationItem, NavigationList {}
