01433ded84d184417820e6c834423ce2fde5c6d9
Chris Meade <chrism@target-technology.co.uk>
2023-05-21 17:35:41 +0100

    temp data

---
+++ temporary_data/navigation.ts
@@ -0,0 +1,16 @@
+const navItems = [
+  { href: "/classes", label: "classes", items: [
+    { href: "/classes/dragons", label: "HD Dragons (Ages 5-6)" },
+    { href: "/classes/children", label: "Children (Ages 7+)" },
+    { href: "/classes/juniors", label: "Juniors (Ages 9+)" },
+    { href: "/classes/Adults", label: "Adults (Ages 16+)" },
+  ] },
+  { href: "/pricing", label: "pricing" },
+  { href: "/timetables", label: "timetables" },
+  { href: "/about", label: "About" },
+  { href: "/faq", label: "faq" },
+  { href: "/shop", label: "shop" },
+  { href: "/contact", label: "contact" },
+];
+
+export default navItems;
