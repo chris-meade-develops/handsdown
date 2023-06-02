9cc542948f083794cab3c128bbad7cf67e47dfa0
Chris Meade <chrism@target-technology.co.uk>
2023-06-02 18:23:45 +0100

    updated types

--- components/form/form.d.ts
+++ components/form/form.d.ts
@@ -1,11 +1,38 @@
-interface Input {
+interface iInput {
   type?: InputHTMLAttributes.type;
   placeholder?: string;
   value?: string;
-  onChange: (e: React.ChangeEvent<T>) => void;
+  onChange?: (e: React.ChangeEvent<T>) => void;
   name: string;
+  label?: string;
 }
 
-interface Radio extends Input {
+interface iRadio extends iInput {
   label: string;
+}
+
+interface iSelect extends iInput {
+  stateSetter: (T) => void;
+  options: { value: string; label: string }[];
+  selectValue: SelectOption;
+}
+interface iSelectOption {
+  value: string;
+  label: string;
+}
+
+interface iFormOptions {
+  type: keyof ComponentMap;
+  props: iInput | iSelect | iRadio;
+}
+
+interface iFormSectionProps {
+  options: FormOptions[];
+}
+
+interface ComponentMap {
+  input: React.ComponentType<iInput>,
+  select: React.ComponentType<iSelect>,
+  checkbox: React.ComponentType<iInput>,
+  radio: React.ComponentType<iRadio>,
 }
