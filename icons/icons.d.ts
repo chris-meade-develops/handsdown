6aeaf015660f2e857ec612f397fee6090761c602
Chris Meade <chrism@target-technology.co.uk>
2023-06-05 18:24:25 +0100

    added a bunch of assets, fixed the hero

--- icons/icons.d.ts
+++ icons/icons.d.ts
@@ -1,3 +1,3 @@
-export type SvgProps = {
+interface SvgProps {
   className?: string;
-};
+}
--- icons/index.ts
+++ icons/index.ts
@@ -1,5 +1,8 @@
 import Phone from "./Phone";
 import Plus from "./Plus";
 import Minus from "./Minus";
+import TakeYourself from "./TakeYourself";
+import ToThe from "./ToThe";
+import NextLevel from "./NextLevel";
 
-export { Phone, Plus, Minus };
+export { Phone, Plus, Minus, TakeYourself, ToThe, NextLevel };
