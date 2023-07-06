bf0967ef32b9d65f0baeeeb36bf2a28b57a63604
Chris Meade <chrism@target-technology.co.uk>
2023-07-06 14:25:21 +0100

    new image, pricing set up heading changed

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -4,19 +4,29 @@ interface iHeading {
   text: string;
   fill: string;
   textColour: string;
+  children?: React.ReactNode;
 }
 
-export default function Heading({ text, fill, textColour }: iHeading) {
+export default function Heading({
+  text,
+  fill,
+  textColour,
+  children,
+}: iHeading) {
   return (
     <div className="relative w-4/5 h-40 mx-auto">
       <HeadingBrush
         className={`absolute z-0 w-full h-full -translate-x-1/2 -translate-y-1/2 left-[54%] top-[29%] min-w-[340px] ${fill}`}
       />
-      <h2
-        className={`relative z-1 max-w-[155px] uppercase font-montserrat font-extrabold text-xl leading-6 text-center ${textColour} -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/4`}
-      >
-        {text}
-      </h2>
+      {children ? (
+        children
+      ) : (
+        <h2
+          className={`relative z-1 max-w-[155px] uppercase font-montserrat font-extrabold text-xl leading-6 text-center ${textColour} -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/4 whitespace-nowrap`}
+        >
+          {text}
+        </h2>
+      )}
     </div>
   );
 }
