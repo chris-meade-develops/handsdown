c846030ac086d10a7415691a25c8e204a1f241c7
Chris Meade <chrism@target-technology.co.uk>
2023-07-19 21:34:13 +0100

    instagram feed

---
+++ components/social/instagramFeed.tsx
@@ -0,0 +1,49 @@
+//this will need to query the instagram api and return the images
+import ImageLink from "@/components/social/imageLink"
+
+const images: iImageLinks[] = [
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+  {
+    image: "/images/michael-grading.jpg",
+    link: "https://www.instagram.com/p/CPQ4ZJ5nZ3I/",
+  },
+]
+
+
+export default function InstagramFeed() {
+  return (
+    <div className="grid grid-cols-2 gap-5 mb-18">
+      {images.map((image, index) => (
+        <ImageLink key={index} image={image.image} link={image.link} />
+      ))}
+    </div>
+  )
+}
+
