62075edae48ce0f6cb3dad63efe244a43b014793
Chris Meade <chrism@target-technology.co.uk>
2023-07-19 21:34:41 +0100

    fixed hydration error

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -1,18 +1,20 @@
-import Primary from "../links/primary";
-import Heading from "../ui/heading";
+import Primary from '../links/primary'
+import Heading from '../ui/heading'
 
 export default function Intro() {
   return (
     <div>
       <Heading text="who we are" fill="fill-primary" textColour="text-white" />
-
-      <p className="text-lg font-normal leading-9 text-center font-montserrat text-primary-text">
+      <p className="mb-10 text-lg font-normal leading-9 text-center font-montserrat text-primary-text">
         Hands Down is a family run martial arts club in Surrey set up by
         brothers Michael and Kalon Page, two high achieving martial arts
-        pedigrees. <br /> <br /> We offer elite training classes at our
-        academies in Epsom and Cobham led by highly accredited and world
-        champion coaches for students of all ages, abilities and training goals
-        .
+        pedigrees.
+      </p>
+      
+      <p className="text-lg font-normal leading-9 text-center font-montserrat text-primary-text">
+        We offer elite training classes at our academies in Epsom and Cobham led
+        by highly accredited and world champion coaches for students of all
+        ages, abilities and training goals.
       </p>
 
       <div className="h-25 w-[270px] mx-auto mt-15 mb-43">
@@ -23,5 +25,5 @@ export default function Intro() {
         </Primary>
       </div>
     </div>
-  );
+  )
 }
