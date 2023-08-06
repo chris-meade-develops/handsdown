2514817762596af64cc81f068cabb668ff0c4b08
Chris Meade <chrism@target-technology.co.uk>
2023-08-06 15:58:44 +0100

    finished hero

--- components/ui/HeroBrush.tsx
+++ components/ui/HeroBrush.tsx
@@ -1,24 +1,30 @@
-import { TakeYourself, ToThe, NextLevel } from "../../icons";
-import Image from "next/image";
+import { TakeYourself, ToThe, NextLevel } from '../../icons'
 
 interface HeroBrushProps {
-  svgOne: string;
-  svgTwo: string;
-  svgThree: string;
+  svgOne: string
+  svgTwo: string
+  svgThree: string
+  svgFour: string
 }
 
 export default function HeroBrush({
   svgOne,
   svgTwo,
   svgThree,
+  svgFour,
 }: HeroBrushProps) {
   return (
     <div className="relative flex flex-col items-center justify-center w-full z-2 xl:mb-11">
       <TakeYourself className={svgOne} />
       <ToThe className={svgTwo} />
       <NextLevel className={svgThree} />
-      
-      <Image src="/images/TakeYourselfTotheNextLevel.png" className="h-auto z-2 " alt="Take yourself to the next level" width={615} height={200} />
+      <NextLevel className={svgFour} />
+
+      <img
+        src="/images/TakeYourselfTotheNextLevel.png"
+        className="h-auto z-2 md:h-55 md:w-[615px] w-[300px]"
+        alt="Take yourself to the next level "
+      />
     </div>
-  );
+  )
 }
