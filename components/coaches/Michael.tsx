2788dd5a569a11dab307264dcc44311448bf9415
Chris Meade <chrism@target-technology.co.uk>
2023-08-06 16:41:51 +0100

    alt

--- components/coaches/Michael.tsx
+++ components/coaches/Michael.tsx
@@ -7,8 +7,8 @@ export default function Michael() {
         src="/images/Michael-cut-out.png"
         width={600}
         height={523}
+        alt="Head coach Michael Page"
       />
-
       <span className="uppercase font-dead-island font-normal text-[25px] tracking-[.75px] leading-[29px] text-white rotate-6 absolute -translate-x-1/2 left-1/2 bottom-50">
         michael page
       </span>
