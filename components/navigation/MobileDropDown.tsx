628fa50d142b8c4bc406db171fb47f3b722fc7f5
Chris Meade <chrism@target-technology.co.uk>
2024-01-15 16:14:58 +0000

    type update

--- components/navigation/MobileDropDown.tsx
+++ components/navigation/MobileDropDown.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 
-export default function MobileDropDown({ open, items }: INavigation.MobileDropDown) {
+export default function MobileDropDown({
+  open,
+  items: { data },
+}: {
+  open: boolean;
+  items: INavigation.Items
+}) {
   if(!open) return null
   return (
     <div className="w-full py-11">
       <ul>
-        {items?.map((item) => (
-          <li key={item.label} className="mb-5">
-            <Link href={item.href} className="text-base font-semibold text-offBlack font-montserrat">{item.label}</Link>
+        {data?.map((item) => (
+          <li key={item.attributes.title} className="mb-5">
+            <Link href={item.attributes.url} className="text-base font-semibold text-offBlack font-montserrat">{item.attributes.title}</Link>
           </li>
         ))}
       </ul>
