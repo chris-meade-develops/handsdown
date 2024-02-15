fd644196b316446a1b577231a1045f2fb10ba7dc
Chris Meade <chrism@target-technology.co.uk>
2024-02-15 11:23:11 +0000

    updated social type

--- components/social/Icons.tsx
+++ components/social/Icons.tsx
@@ -1,17 +1,36 @@
-import { Twitter, Instagram, Facebook } from "../../icons";
+import Link from 'next/link'
+import { Twitter, Instagram, Facebook } from '../../icons'
 
-export default function Icons() {
+const iconMap: { [key: string]: any } = {
+  facebook: (href: string) => (
+    <Link href={href} className="w-auto group h-15 md:h-16">
+      <Facebook className="w-full h-full fill-white group-hover:fill-accent" />
+    </Link>
+  ),
+  instagram: (href: string) => (
+    <Link href={href} className="w-auto group h-19 md:h-22 ml-25 mr-25">
+      <Instagram className="w-full h-full fill-white group-hover:fill-accent" />
+    </Link>
+  ),
+  twitter: (href: string) => (
+    <Link href={href} className="w-auto group h-19 md:h-22">
+      <Twitter className="w-full h-full fill-white group-hover:fill-accent" />
+    </Link>
+  ),
+}
+
+export default function Icons(platforms: {
+  platforms: { id: string; Platform: string; Link: string }[]
+}) {
   return (
     <div className="flex items-center justify-center w-full mb-22">
-      <div className="w-auto h-15 md:h-16">
-        <Facebook className="w-full h-full fill-white" />
-      </div>
-      <div className="w-auto h-19 md:h-22 ml-25 mr-25">
-        <Instagram className="w-full h-full fill-white" />
-      </div>
-      <div className="w-auto h-19 md:h-22">
-        <Twitter className="w-full h-full fill-white" />
-      </div>
+      {platforms.platforms.map((platform) => {
+        const { Link: link, Platform: platformName, id } = platform
+        const Icon = iconMap[platformName.toLowerCase()]
+        return (
+          <Icon key={id} className="w-full h-full fill-white" href={link} />
+        )
+      })}
     </div>
-  );
+  )
 }
--- components/social/InstagramFeed.tsx
+++ components/social/InstagramFeed.tsx
@@ -7,11 +7,11 @@ import Section from '../section/Section'
 import Heading from '../ui/Heading'
 import Icons from './Icons'
 
-export default async function InstagramFeed() {
+export default async function InstagramFeed(data: ICms.DynamicComponents) {
   const instagramAccessData = await checkTokenExpiry()
 
   if (!instagramAccessData.success || !instagramAccessData.data) {
-    return <Error />
+    return <Error {...data}  />
   }
 
   const { accessToken, userId } = instagramAccessData.data.attributes
@@ -25,7 +25,7 @@ export default async function InstagramFeed() {
 
   if (!instagramPosts.success || !instagramPosts.data) {
     return (
-      <Error />
+      <Error {...data} />
     )
   }
 
@@ -38,8 +38,8 @@ export default async function InstagramFeed() {
       bgColor="bg-secondary"
       className="relative text-base font-medium leading-7 bg-primary py-23 font-montserrat text-tertiary-text"
     >
-      <Heading fill="fill-primary" text="Follow us" textColour="text-white" />
-      <Icons />
+      <Heading fill="fill-primary" text={data.Title} textColour="text-white" />
+      <Icons platforms={data.Platforms} />
       <div className="grid grid-cols-2 gap-5 mb-18 md:grid-cols-3 md:gap-10 md:px-53">
         {filteredPosts.map((image, index) => (
           <ImageLink key={image.id} {...image} />
@@ -49,7 +49,7 @@ export default async function InstagramFeed() {
   )
 }
 
-const Error = () => (
+const Error = (data: ICms.DynamicComponents) => (
   <Section
   bgColor="bg-secondary"
   className="relative text-base font-medium leading-7 text-center bg-primary py-23 font-montserrat text-tertiary-text"
@@ -60,6 +60,6 @@ const Error = () => (
     There has been an error grabbing our latest social media posts, click
     one of the links below to find us:{' '}
   </h1>
-  <Icons />
+  <Icons platforms={data.Platforms} />
 </Section>
 )
