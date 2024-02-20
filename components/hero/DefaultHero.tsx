ba27314d45270623fdce6398b19cbf888411a23f
Chris Meade <chrism@target-technology.co.uk>
2024-02-20 20:02:37 +0000

    fixed the brush on the hero

--- components/hero/DefaultHero.tsx
+++ components/hero/DefaultHero.tsx
@@ -16,14 +16,14 @@ export default function DefaultHero(heroData: ICms.Hero) {
       <div className="absolute top-0 w-full h-full bg-offBlack/50 z-2" />
 
       <div className="relative mx-auto mb-10 w-fit">
-        <MeetTheFounders className="absolute w-[150%] mx-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-3" />
-        <h1 className="text-[42px] text-white font-montserrat z-3 relative leading-[100px] font-extrabold uppercase whitespace-nowrap mx-5">
+        <MeetTheFounders className="absolute w-[230%] max-w-[100vw] mx-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 z-3" />
+        <h1 className=" text-4xl md:text-[42px] text-white font-montserrat z-3 relative leading-[100px] font-extrabold uppercase whitespace-nowrap mx-5">
           {heroData.data.attributes.title}
         </h1>
       </div>
 
       <div className="max-w-[565px] mx-auto px-10 md:px-0">
-        <p className="relative text-lg font-medium leading-6 text-center text-white font-montserrat z-3">
+        <p className="relative text-sm font-medium leading-6 text-center text-white md:text-lg font-montserrat z-3">
           {heroData.data.attributes.description}
         </p>
       </div>
@@ -36,7 +36,7 @@ export default function DefaultHero(heroData: ICms.Hero) {
               ?.scrollIntoView({ behavior: 'smooth' })
           }}
         >
-          <span className="text-base font-bold leading-6 text-white uppercase font-montserrat">
+          <span className="text-sm font-bold leading-6 text-white uppercase  md:text-base font-montserrat">
             {heroData.data.attributes.link.text}
           </span>
         </Primary>
