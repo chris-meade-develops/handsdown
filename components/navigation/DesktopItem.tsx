936d67802b0cf48c4b735a9aa423605739f4e0c2
Chris Meade <chrism@target-technology.co.uk>
2024-02-21 21:59:46 +0000

    fixed 404

--- components/navigation/DesktopItem.tsx
+++ components/navigation/DesktopItem.tsx
@@ -1,16 +1,16 @@
 import Link from 'next/link'
 import DesktopDropDown from './DesktopDropDown'
 
-export default function DesktopItem({ attributes: {
-  url,
-  title,
-  children
-} }: INavigation.Item) {
+export default function DesktopItem({
+  attributes: { url, title, children },
+}: INavigation.Item) {
   return (
-    <li
-      className="relative text-base font-semibold leading-5 tracking-widest uppercase font-montserrat group hover:text-accent"
-    >
-      <Link href={url}>{title}</Link>
+    <li className="relative text-base font-semibold leading-5 tracking-widest uppercase font-montserrat group hover:text-accent">
+      {title.toLowerCase() === 'shop' ? (
+        <span className='cursor-pointer'>{title}</span>
+      ) : (
+        <Link href={url}>{title}</Link>
+      )}
 
       {children && <DesktopDropDown items={children} />}
     </li>
