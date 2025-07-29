00abe7755e05bd38b410dfe05abfdf8b3aef3706
Chris Meade <chrism@target-technology.co.uk>
2025-07-29 14:41:01 +0100

    added anouncement bar to navigation

--- components/navigation/Desktop.tsx
+++ components/navigation/Desktop.tsx
@@ -1,54 +1,67 @@
 'use client'
-import { useEffect, useState } from 'react'
+
 import DesktopItem from './DesktopItem'
 import Image from 'next/image'
+import AnnouncementBar from '../announcement-bar'
 
 export default function Desktop({
   scrolled,
   navItems,
+  promoBlock,
 }: {
   scrolled: boolean
   navItems: INavigation.Items
+  promoBlock: ICms.Announcement | null
 }) {
   const firstHalf = navItems.data.slice(0, 3)
   const secondHalf = navItems.data.slice(3, 8)
 
   return (
     <nav
-      className={`fixed top-0 left-0 justify-between hidden w-full  md:flex transition-all duration-300 md:mb-14 z-2 ${
+      className={`fixed top-0 left-0  hidden w-full md:block transition-all duration-300 md:mb-14 z-2 ${
         scrolled
-          ? 'bg-secondary md:pl-32 md:pr-41 text-primary-text'
-          : 'bg-transparent md:py-21 md:pr-41 md:pl-46 text-secondary-text '
+          ? 'bg-secondary  text-primary-text'
+          : 'bg-transparent  text-secondary-text '
       }`}
     >
-      <ul className="flex justify-between gap-3 list-none w-1/3 max-w-[468px] items-center">
-        {firstHalf.map((item) => (
-          <DesktopItem
-            key={item.id}
-            id={item.id}
-            attributes={item.attributes}
-          />
-        ))}
-      </ul>
-      <div className={`transition-all duration-300 ${scrolled ? 'my-8' : ''}`}>
-        <a href="/" className={`${scrolled ? 'block' : 'hidden'}`}>
-          <Image
-            src="https://res.cloudinary.com/dkf2b6ta5/image/upload/v1702322316/nav_logo_a34c0a09de.png"
-            width={116}
-            height={71}
-            alt="Handsdown logo"
-          />
-        </a>
+      {promoBlock && !scrolled && <AnnouncementBar promoBlock={promoBlock} />}
+      <div
+        className={`flex justify-between ${
+          scrolled ? 'md:pl-32 md:pr-41' : 'md:py-21 md:pr-41 md:pl-46'
+        }`}
+      >
+        <ul className="flex justify-between gap-3 list-none w-1/3 max-w-[468px] items-center">
+          {firstHalf.map((item) => (
+            <DesktopItem
+              key={item.id}
+              id={item.id}
+              attributes={item.attributes}
+            />
+          ))}
+        </ul>
+        <div
+          className={`transition-all duration-300 ${scrolled ? 'my-8' : ''}`}
+        >
+          <a href="/" className={`${scrolled ? 'block' : 'hidden'}`}>
+            <Image
+              src="https://res.cloudinary.com/dkf2b6ta5/image/upload/v1702322316/nav_logo_a34c0a09de.png"
+              width={116}
+              height={71}
+              alt="Handsdown logo"
+            />
+          </a>
+        </div>
+        <ul className="flex justify-between list-none max-w-[478px] w-1/3 items-center gap-3">
+          {secondHalf.map((item) => (
+            <DesktopItem
+              key={item.id}
+              id={item.id}
+              attributes={item.attributes}
+            />
+          ))}
+        </ul>
       </div>
-      <ul className="flex justify-between list-none max-w-[478px] w-1/3 items-center gap-3">
-        {secondHalf.map((item) => (
-          <DesktopItem
-            key={item.id}
-            id={item.id}
-            attributes={item.attributes}
-          />
-        ))}
-      </ul>
+      {promoBlock && scrolled && <AnnouncementBar promoBlock={promoBlock} />}
     </nav>
   )
 }
--- components/navigation/Mobile.tsx
+++ components/navigation/Mobile.tsx
@@ -4,11 +4,13 @@ import BurgerMenu from './BurgerMenu'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import AnnouncementBar from '../announcement-bar'
 
 export default function Mobile({
   open,
   onClick,
   scrolled,
+  promoBlock,
 }: INavigation.MobileNav) {
   const [img, setImg] = useState('')
 
@@ -27,33 +29,36 @@ export default function Mobile({
     fetchImg()
   }, [])
   return (
-    <nav
-      className={`fixed top-0 left-0 w-full md:hidden px-14 z-3 ${
-        scrolled
-          ? 'bg-secondary text-primary-text py-10'
-          : 'bg-transparent text-secondary-text py-20  '
-      } transition-all duration-300
-    }`}
-    >
-      <ul className="flex items-center justify-between list-none">
-        <li>
-          <BurgerMenu scrolled={scrolled} open={open} onClick={onClick} />
-        </li>
-        {scrolled && !open && (
-          <Link href="/">
-            <Image src={img} alt="nav logo" width={79} height={42} />
-          </Link>
-        )}
-        <li>
-          <a href="tel:03300589474" className="block w-13 h-13">
-            <Phone
-              className={`w-full h-full -rotate-12 transition-colors duration-300 ${
-                open || scrolled ? 'fill-offBlack' : 'fill-secondary-text'
-              }`}
-            />
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className="fixed top-0 left-0 w-full md:hidden z-3 ">
+      {promoBlock && <AnnouncementBar promoBlock={promoBlock} />}
+      <nav
+        className={`w-full  px-14 ${
+          scrolled
+            ? 'bg-secondary text-primary-text py-10'
+            : 'bg-transparent text-secondary-text py-20  '
+        } transition-all duration-300
+      }`}
+      >
+        <ul className="flex items-center justify-between list-none">
+          <li>
+            <BurgerMenu scrolled={scrolled} open={open} onClick={onClick} />
+          </li>
+          {scrolled && !open && img !== '' && (
+            <Link href="/">
+              <Image src={img} alt="nav logo" width={79} height={42} />
+            </Link>
+          )}
+          <li>
+            <a href="tel:03300589474" className="block w-13 h-13">
+              <Phone
+                className={`w-full h-full -rotate-12 transition-colors duration-300 ${
+                  open || scrolled ? 'fill-offBlack' : 'fill-secondary-text'
+                }`}
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
